4ba4d380b6fb5afa410c1c6a9b3765ece4616baf
ViljarBakken <viljar.bakken@gmail.com>
2018-10-12 10:56:37 +0200

    forste commit

---
+++ Assets/Scripts/BallScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour {
+
+    public Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+        
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Die()
+    {
+        Destroy(gameObject);
+        GameObject paddleObject = GameObject.Find("paddle");
+        PaddleScript paddleScript = paddleObject.GetComponent<PaddleScript>();
+        paddleScript.SpawnBall();
+    }
+}
---
+++ Assets/Scripts/PaddleScript.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleScript : MonoBehaviour
+{
+    
+    public Collider cl;
+    float PaddleSpeed = 10f;
+    public GameObject ballPrefab;
+    // public Rigidbody rb;
+
+    int lives = 4;
+
+    GameObject attachedBall = null;
+
+    // Use this for initialization
+    void Start()
+    {
+        SpawnBall();
+
+    }
+    public void SpawnBall()
+    {
+        attachedBall = (GameObject)Instantiate(ballPrefab, transform.position + new Vector3(0, .75f, 0), Quaternion.identity);
+        lives--;
+
+        GUIText guiLives = GameObject.Find("guiLives").GetComponent<GUIText>();
+        guiLives.text = "Lives: " + lives;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //Left-Right movement
+        transform.Translate(PaddleSpeed * Time.deltaTime * Input.GetAxis("Horizontal"), 0, 0);
+        //rb = GetComponent<Rigidbody>();
+        // ballRigidbody = attachedBall.GetComponent<Rigidbody>();
+        if (attachedBall)
+        {
+            Rigidbody ballRigidbody = attachedBall.GetComponent<Rigidbody>();
+            ballRigidbody.position = transform.position + new Vector3(0, .75f, 0);
+
+            if (Input.GetButtonDown( "LaunchBall"))
+        {
+                //Fire the ball
+
+                ballRigidbody.isKinematic = false;
+                ballRigidbody.AddForce(300f * Input.GetAxis("Horizontal"), 300f, 0);
+                attachedBall = null;
+            }
+            }
+
+    }
+    void FixedUpdate()
+    {
+
+    }
+    void OnCollisionEnter (Collision col)
+    {
+        cl = GetComponent<Collider>();
+        
+        foreach (ContactPoint contact in col.contacts)
+        {
+           if ( contact.thisCollider == cl )
+            {
+                //This is the paddle's contact point
+               float english = contact.point.x - transform.position.x;
+                contact.otherCollider.GetComponent<Rigidbody>().AddForce(300f * english, 0, 0);
+            }
+        }
+    }
+}
